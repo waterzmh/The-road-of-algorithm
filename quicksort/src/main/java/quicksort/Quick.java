dc6f5d088607765ebc07eafc1d8523203ebd5bb0
zhuminhao <903936820@qq.com>
2018-05-04 09:57:06 +0800

    add genericity

--- quicksort/src/main/java/quicksort/Quick.java
+++ quicksort/src/main/java/quicksort/Quick.java
@@ -8,8 +8,9 @@ import java.util.List;
  * @createtime: 2018/5/3
  */
 
-public class Quick<T extends Number>{
-    public void sort(List<Integer> tList,int begin,int end){
+public class Quick{
+    //继承Comparable接口才可以比较  不然无法比
+    public <T extends Comparable<T>> void sort(List<T> tList,int begin,int end){
         if(begin>=end){
             return;
         }
@@ -19,14 +20,15 @@ public class Quick<T extends Number>{
         //设置要确序的数组下标
         int base=begin;
         //设置中间变量
-        int tmp;
+        T tmp;
         //设置左端or右端移动的标志,因为基数是左端，所以默认必须右端开始移动
         int left=-1;
         while(begin<end){
             //如果是右端在移动
             if(left==-1){
                 //如果右端的值小于中间变量的值，则互换位置
-                if(tList.get(end) < tList.get(base)){
+                //自定义类的话要重写compareTo
+                if(tList.get(end).compareTo(tList.get(base))<0){
                     tmp=tList.get(base);
                     tList.set(base,tList.get(end));
                     tList.set(end,tmp);
@@ -37,7 +39,7 @@ public class Quick<T extends Number>{
                 end--;
             }else {
                 //如果左端的值大于中间变量的值，则互换位置
-                if(tList.get(begin) > tList.get(base) ){
+                if(tList.get(begin).compareTo(tList.get(base))>0 ){
                     tmp=tList.get(base);
                     tList.set(base,tList.get(begin));
                     tList.set(begin,tmp);
@@ -50,6 +52,5 @@ public class Quick<T extends Number>{
         }
         sort(tList,beginLeft,base-1);
         sort(tList,base+1,beginRight);
-
     }
 }
--- quicksort/src/test/java/Test.java
+++ quicksort/src/test/java/Test.java
@@ -1,6 +1,7 @@
 import quicksort.Quick;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
